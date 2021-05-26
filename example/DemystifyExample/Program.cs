c90c8d16fee133c045e62ba6a1b3db87465e554d
Jordy Hulck <Jordy.Hulck@outlook.com>
2021-05-26 19:48:50 +0200

    Upgrade example to .NET 5.0

--- example/DemystifyExample/Program.cs
+++ example/DemystifyExample/Program.cs
@@ -5,39 +5,30 @@ using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Serilog;
 
-namespace DemystifyExample
+Log.Logger = new LoggerConfiguration()
+    .Enrich.WithDemystifiedStackTraces()
+    .WriteTo.Console()
+    .CreateLogger();
+
+try
 {
-    class Program
-    {
-        static async Task<int> Main()
-        {
-            Log.Logger = new LoggerConfiguration()
-                .Enrich.WithDemystifiedStackTraces()
-                .WriteTo.Console()
-                .CreateLogger();
+    return await FailingMethodAsync();
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "Unhandled exception");
+    return 1;
+}
 
-            try
-            {
-                return await FailingMethodAsync();
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "Unhandled exception");
-                return 1;
-            }
-        }
+[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+static async Task<int> FailingMethodAsync()
+{
+    return await Task.FromResult(FailingEnumerator().Sum());
+}
 
-        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-        static async Task<int> FailingMethodAsync()
-        {
-            return await Task.FromResult(FailingEnumerator().Sum());
-        }
-
-        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-        static IEnumerable<int> FailingEnumerator()
-        {
-            yield return 1;
-            throw new NotImplementedException();
-        }
-    }
+[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+static IEnumerable<int> FailingEnumerator()
+{
+    yield return 1;
+    throw new NotImplementedException();
 }
